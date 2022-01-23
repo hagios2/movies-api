1a4de220556309521b8faeb7efe802a921ad35bf
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-01-23 13:43:45 +0000

    finished adding test for the endpoints

--- src/Schemas/CommentRequestSchema.js
+++ src/Schemas/CommentRequestSchema.js
@@ -2,7 +2,7 @@ import { object, string } from 'yup'
 
 const commentRequestSchema = object({
   body: object({
-    comment: string().required('Comment is required').max(500, 'Comment should be limited to 500 characters')
+    comment: string().required('Comment is required').max(500, 'Comment should be limited to 500 words')
   })
 })
 
--- src/_tests_/movie_api.test.js
+++ src/_tests_/movie_api.test.js
@@ -1,10 +1,10 @@
 import supertest from 'supertest'
 import { app } from '../app.js'
 import { testDbConnect, testDbClose } from '../config/test_db'
-import { Comment } from '../Models/Comment.js'
+import faker from 'faker'
+
 
 beforeAll(async () => await testDbConnect())
-// beforeEach(async () => await testDbClear())
 afterAll(async () => await testDbClose())
 
 describe('starwars movie api', () => {
@@ -16,101 +16,81 @@ describe('starwars movie api', () => {
       expect(response.statusCode).toBe(200)
     })
 
-  //   it('should expect response body _id to be defined', async () => {
-  //     const department = 'Platforms'
-  //     const response = await supertest(app).post('/api/admin/create/department').send({
-  //       name: department
-  //     })
+    it('should expect response body data to be defined', async () => {
+      const response = await supertest(app).get('/api/fetch/movies').send({})
 
-  //     expect(response.body.data._id).toBeDefined()
-  //   })
+      expect(response.body.data).toBeDefined()
+      expect(Array.isArray(response.body.data)).toBeTruthy()
+      expect(Object.keys(response.body.data[0]).sort()).toEqual(['title', 'comments', 'filmId', 'release_date', 'opening_crawl'].sort())
+    })
+  })
 
-  //   it('should expect response body name to be equal to department', async () => {
-  //     const department = 'Platforms'
-  //     const response = await supertest(app).post('/api/admin/create/department').send({
-  //       name: department
-  //     })
+  describe('POST /api/add/movie/:filmId/comment', () => {
+    it('should validate comment as required if not provided', async () => {
+      const response = await supertest(app).post('/api/add/movie/1/comment').send({})
 
-  //     expect(response.body.data.name).toEqual(department)
-  //   })
+      expect(response.statusCode).toBe(422)
 
-  //   it('should respond with status 422 if name is not provided', async () => {
-  //     const response = await supertest(app).post('/api/admin/create/department').send({})
+      expect(response.body).toMatchObject({
+        error: 'Comment is required'
+      })
+    })
 
-  //     expect(response.statusCode).toBe(422)
-  //   })
-  // })
+    it('should validate comment should be limited to 500 words', async () => {
+      const comment = faker.lorem.paragraphs()
 
-  // describe('GET /api/admin/get/departments', () => {
-  //   it('should respond with 403 status when not authenticated', async () => {
-  //     const response = await supertest(app).get('/api/admin/get/departments')
+      const response = await supertest(app).post('/api/add/movie/1/comment').send({comment})
 
-  //     expect(response.statusCode).toBe(403)
-  //   })
+      expect(response.statusCode).toBe(422)
+      
+      expect(response.body).toMatchObject({
+        error: 'Comment should be limited to 500 words'
+      })
+    })
 
-  //   it('should respond with 200 status', async () => {
-  //     const department = await Department.create({ name: 'Platform' })
-  //     const user = {
-  //       name: 'Mamphey',
-  //       email: 'hagioswilson@gmail.com',
-  //       password: '123456789',
-  //       departmentId: department._id
-  //     }
+    it('should comment should be saved when provided', async () => {
+      const comment = faker.lorem.words(30)
+      const response = await supertest(app).post('/api/add/movie/1/comment').send({comment})
 
-  //     await supertest(app).post('/api/auth/register').send(user)
+      expect(response.statusCode).toBe(201)
 
-  //     const loginResponse = await supertest(app).post('/api/auth/login').send({
-  //       email: user.email,
-  //       password: user.password
-  //     })
+      expect(response.body).toMatchObject({
+        message: 'comment added',
+        data: {}
+      })
+    })
+  })
 
-  //     const response = await supertest(app).get('/api/admin/get/departments').set('Authorization', `Bearer ${loginResponse.body.data.token}`)
+  describe('GET /api/fetch/movie/:filmId/comments', () => {
+    it('should respond with status code 200', async () => {
+      const response = await supertest(app).get('/api/fetch/movie/1/comments')
 
-  //     expect(response.statusCode).toBe(200)
-  //   })
-  // })
+      expect(response.statusCode).toBe(200)
+    })
 
-  // describe('GET api/admin/department/:departmentId/fetch', () => {
-  //   it('should respond with 200 status', async () => {
-  //     const department = await Department.create({ name: 'Platform' })
-  //     const user = {
-  //       name: 'Mamphey',
-  //       email: 'hagioswilson@gmail.com',
-  //       password: '123456789',
-  //       departmentId: department._id
-  //     }
+    it('should respond with data defined as an arrary', async () => {
+      const response = await supertest(app).get('/api/fetch/movie/1/comments')
 
-  //     await supertest(app).post('/api/auth/register').send(user)
+      expect(response.body.data).toBeDefined()
+      expect(Array.isArray(response.body.data)).toBeTruthy()
+      expect(Object.keys(response.body.data[0]).sort()).toEqual(['id', 'filmId', 'comment', 'ip_address', 'createdAt', 'updatedAt'].sort())
+    })
+  })
 
-  //     const loginResponse = await supertest(app).post('/api/auth/login').send({
-  //       email: user.email,
-  //       password: user.password
-  //     })
+  describe('GET /api/fetch/movie/:filmId/characters', () => {
+    it('should respond with 200 status', async () => {
+      const response = await supertest(app).get('/api/fetch/movie/1/characters').send({})
 
-  //     const response = await supertest(app).get(`/api/admin/department/${department._id}/fetch`).set('Authorization', `Bearer ${loginResponse.body.data.token}`)
+      expect(response.statusCode).toBe(200)
+    })
 
-  //     expect(response.statusCode).toBe(200)
-  //   })
+    it('should expect resonse data to be defined', async () => {
+      const response = await supertest(app).get('/api/fetch/movie/1/characters').send({})
 
-  //   it('should expect  resonse data to be defined', async () => {
-  //     const department = await Department.create({ name: 'Platform' })
-  //     const user = {
-  //       name: 'Mamphey',
-  //       email: 'hagioswilson@gmail.com',
-  //       password: '123456789',
-  //       departmentId: department._id
-  //     }
-
-  //     await supertest(app).post('/api/auth/register').send(user)
-
-  //     const loginResponse = await supertest(app).post('/api/auth/login').send({
-  //       email: user.email,
-  //       password: user.password
-  //     })
-
-  //     const response = await supertest(app).get(`/api/admin/department/${department._id}/fetch`).set('Authorization', `Bearer ${loginResponse.body.data.token}`)
-
-  //     expect(response.body.data).toBeDefined()
-  //   })
+      expect(response.body.data).toBeDefined()
+      expect(Object.keys(response.body.data).sort()).toEqual(['characterList', 'metadata'].sort())
+      expect(Object.keys(response.body.data.characterList[0]).sort()).toEqual(['characterId', 'name', 'gender', 'height'].sort())
+      expect(Object.keys(response.body.data.metadata).sort()).toEqual(['totalNumberOfCharacters', 'totalHeightOfCharactersInFeet', 'totalHeightOfCharactersInInches'].sort())
+    })
   })
 })
